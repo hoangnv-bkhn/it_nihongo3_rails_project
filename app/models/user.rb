6563e1d807d774ed692bae16acc247e59bfd8cad
hoangnv <viethoang.ma1009@gmail.com>
2021-12-10 17:26:44 +0000

    Add Discussion Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-    has_many :discussions, dependent :destroy
-    has_many :channels, through: :discussions
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :discussions, dependent: :destroy
+  has_many :channels, through: :discussions
+
 end
