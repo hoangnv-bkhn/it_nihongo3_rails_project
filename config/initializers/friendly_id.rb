2745c4337bdc838b4bb57bcb9da02578471c38d6
Vu Van Trong <57039177+trongvuvan@users.noreply.github.com>
2021-12-12 01:23:50 +0700

    completed

--- config/initializers/friendly_id.rb
+++ config/initializers/friendly_id.rb
@@ -3,7 +3,7 @@
 # Use this to set up shared configuration options for your entire application.
 # Any of the configuration options shown here can also be applied to single
 # models by passing arguments to the `friendly_id` class method or defining
-# methods in your model.
+# methods in your model..
 #
 # To learn more, check out the guide:
 #
