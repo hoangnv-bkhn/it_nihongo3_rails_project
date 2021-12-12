a36763d43e984d1596b056b4b1c48ef64f3f4574
trongvuvan <trong.vuvan0810@gmail.com>
2021-12-12 01:49:06 +0000

    completed

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,14 +1,16 @@
 class DiscussionsController < ApplicationController
   before_action :set_discussion, only: [:show, :edit, :update, :destroy]
-  before_action :find_channels, only: [:index, :show, :new, :edit ]
+  before_action :find_channels, only: [:index, :show, :new, :edit]
   before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /discussions or /discussions.json
+  # GET /discussions
+  # GET /discussions.json
   def index
     @discussions = Discussion.all.order('created_at desc')
   end
 
-  # GET /discussions/1 or /discussions/1.json
+  # GET /discussions/1
+  # GET /discussions/1.json
   def show
     @discussions = Discussion.all.order('created_at desc')
   end
@@ -22,39 +24,42 @@ class DiscussionsController < ApplicationController
   def edit
   end
 
-  # POST /discussions or /discussions.json
+  # POST /discussions
+  # POST /discussions.json
   def create
     @discussion = current_user.discussions.build(discussion_params)
 
     respond_to do |format|
       if @discussion.save
-        format.html { redirect_to @discussion, notice: "Discussion was successfully created." }
+        format.html { redirect_to @discussion, notice: 'Discussion was successfully created.' }
         format.json { render :show, status: :created, location: @discussion }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @discussion.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /discussions/1 or /discussions/1.json
+  # PATCH/PUT /discussions/1
+  # PATCH/PUT /discussions/1.json
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
-        format.html { redirect_to @discussion, notice: "Discussion was successfully updated." }
+        format.html { redirect_to @discussion, notice: 'Discussion was successfully updated.' }
         format.json { render :show, status: :ok, location: @discussion }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @discussion.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /discussions/1 or /discussions/1.json
+  # DELETE /discussions/1
+  # DELETE /discussions/1.json
   def destroy
     @discussion.destroy
     respond_to do |format|
-      format.html { redirect_to discussions_url, notice: "Discussion was successfully destroyed." }
+      format.html { redirect_to discussions_url, notice: 'Discussion was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -64,13 +69,12 @@ class DiscussionsController < ApplicationController
     def set_discussion
       @discussion = Discussion.find(params[:id])
     end
-    
+
     def find_channels
-        @channels = Channel.all.order('created_at desc')
-end
+      @channels = Channel.all.order('created_at desc')
+    end
 
-
-    # Only allow a list of trusted parameters through.
+    # Never trust parameters from the scary internet, only allow the white list through.
     def discussion_params
       params.require(:discussion).permit(:title, :content, :channel_id)
     end
